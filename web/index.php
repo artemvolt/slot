58e77cd13cd9ecb181b8325d9d1f66c1b9a2f304
Artem Volt <avoltornistyy@beeline.ru>
2022-10-28 08:37:01 +0300

    Remove unused class.

--- web/index.php
+++ web/index.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL);
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Slotegrator\App\Components\RequestHandler;
 use Symfony\Component\HttpFoundation\Request;
 
 $request = Request::createFromGlobals();
